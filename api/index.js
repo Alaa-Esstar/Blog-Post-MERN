1927c7fe4c8014149c174897a37c15f942a2202d
Alaa Esstar <alaa.esstar@gmail.com>
2023-07-30 19:39:45 +0100

    fix upload error

--- api/index.js
+++ api/index.js
@@ -9,11 +9,17 @@ const categoryRoute = require("./routes/categories");
 const multer = require('multer');
 const cors = require('cors');
 const path = require("path");
+const fs = require("fs");
 
 dotenv.config();
 app.use(express.json());
 app.use(cors());
-app.use("/images", express.static(path.join(__dirname, "/images")))
+// Create the /images directory if it doesn't exist
+const imagesDirectory = path.join(__dirname, "/images");
+if (!fs.existsSync(imagesDirectory)) {
+    fs.mkdirSync(imagesDirectory);
+}
+app.use("/images", express.static(imagesDirectory));
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
